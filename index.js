53c5ed63896fe64e9e80f95930de75d3f5689fd3
døve <104696625+doveACE@users.noreply.github.com>
2022-06-14 22:44:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let SelectedItems = [];
 let ItemDisplays = [];
 let LoginToken = localStorage.getItem("Token")
 
-if (window.location.href.split("&") && window.location.href.split("&")[1].substr(13)) {
+if (window.location.href.split("&")[1] && window.location.href.split("&")[1].substr(13)) {
   localStorage.setItem("Token", window.location.href.split("&")[1].substr(13))
   window.location.href = window.location
 }
